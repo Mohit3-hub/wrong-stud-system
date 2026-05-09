a9398602fd0e079eca2aeafc47aab0489f3f7af2
Mohit3-hub <mohitbhivare333@gmail.com>
2026-05-09 08:16:45 +0530

    login page created

--- js/login.js
+++ js/login.js
@@ -1,1 +1,21 @@
+function loginOperator() {
 
+    const employeeId = document
+        .getElementById("employeeId")
+        .value
+        .trim();
+
+    const errorMessage = document.getElementById("errorMessage");
+
+    if (employeeId === "") {
+
+        errorMessage.innerText = "Please enter Employee ID";
+        return;
+    }
+
+    // Save login session
+    localStorage.setItem("employeeId", employeeId);
+
+    // Redirect to home screen
+    window.location.href = "home.html";
+}
